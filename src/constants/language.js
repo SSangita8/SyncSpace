0054b3cbde8e2c96d49ac287a3316c9bd1e84dad
Sushree Sangita <147142711+SSangita8@users.noreply.github.com>
2026-08-11 22:43:48 +0530

    Create language.js with supported programming languages
    Added a list of programming languages with their IDs and Monaco identifiers.

---
+++ src/constants/language.js
@@ -0,0 +1,52 @@
+export const LANGUAGES = [
+  {
+    id: 63,
+    name: "JavaScript",
+    monaco: "javascript",
+  },
+  {
+    id: 71,
+    name: "Python",
+    monaco: "python",
+  },
+  {
+    id: 62,
+    name: "Java",
+    monaco: "java",
+  },
+  {
+    id: 54,
+    name: "C++",
+    monaco: "cpp",
+  },
+  {
+    id: 50,
+    name: "C",
+    monaco: "c",
+  },
+  {
+    id: 51,
+    name: "C#",
+    monaco: "csharp",
+  },
+  {
+    id: 60,
+    name: "Go",
+    monaco: "go",
+  },
+  {
+    id: 73,
+    name: "Rust",
+    monaco: "rust",
+  },
+  {
+    id: 68,
+    name: "PHP",
+    monaco: "php",
+  },
+  {
+    id: 72,
+    name: "Ruby",
+    monaco: "ruby",
+  },
+];
